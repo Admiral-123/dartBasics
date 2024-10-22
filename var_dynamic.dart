3ca0280dbbf7ce3c55f72ceb2d3907bfaef3ab03
admiral <kartikeyk1776@gmail.com>
2024-10-22 18:30:53 +0530

    add var_dynamic

---
+++ var_dynamic.dart
@@ -0,0 +1,18 @@
+void main() {
+  // var
+
+  var section;
+
+  section = 'D';
+  section = 6;
+  section = false;
+  // var without initial value allows reassignment with any type
+
+  var rollno = 12;
+  // it is assigned with int so rollno has became int (dynamic) type
+
+  // rollno = 's'; // Error: rollno was assigned an int, so it cannot hold a String
+
+  print(section);
+  print(rollno);
+}
