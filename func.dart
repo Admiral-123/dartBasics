fd0660fe954e7a212cf3670a0a59bc88daaa9f88
admiral <kartikeyk1776@gmail.com>
2024-10-24 17:49:56 +0530

    functions

--- func.dart
+++ func.dart
@@ -1,10 +1,25 @@
+import 'dart:ffi';
+
 void main() {
   var a = new myClass();
   a.printName('varun');
+
+  var x = myClass.multiply(5, 7);
+  print(x);
 }
 
 class myClass {
+  myClass() {
+    // default constructor
+    print(
+        'constructor of the class'); // constructor is always called first when we create obj of class
+  }
+
   void printName(var name) {
     print(name);
+  }
+
+  static int multiply(int a, int b) {
+    return a * b;
   }
 }
