0864b2a0ab60fcb53274f4f106bebc8f5d1857e9
admiral <kartikeyk1776@gmail.com>
2024-10-26 19:48:40 +0530

    about text

---
+++ flutter-lib-main/text.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Entry point of the Flutter application
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // The root widget of the application, MyApp sets up the basic MaterialApp and theme
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Text',
+      debugShowCheckedModeBanner: false, // Remove debug banner from the app
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter text'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // Stateful widget to manage state (like the counter) within the page
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0; // Variable to keep track of the counter
+
+  // Function to increment counter, calls setState to trigger a UI update
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Text('hlo',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.blue,
+                backgroundColor: Colors.amber,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic)));
+  }
+}
+
+/*
+In Flutter, the Text widget is a built-in widget that allows you to display a
+string of text within your app’s UI. It’s one of the most commonly used widgets,
+as it supports styling and formatting to create different visual effects and
+alignments. 
+*/
