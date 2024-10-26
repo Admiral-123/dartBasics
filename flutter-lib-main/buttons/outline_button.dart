64be237e50ee9aee1cf3ae8d22f6d0e41e6089d2
admiral <kartikeyk1776@gmail.com>
2024-10-26 21:08:22 +0530

    about buttons

---
+++ flutter-lib-main/buttons/outline_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Entry point of the Flutter application
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // The root widget of the application, MyApp sets up the basic MaterialApp and theme
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter OutlinedButton',
+      debugShowCheckedModeBanner: false, // Remove debug banner from the app
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter outlined-button'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // Stateful widget to manage state (like the counter) within the page
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: ElevatedButton(
+            onPressed: () {
+              print('clicked elvated button');
+            },
+            onLongPress: () {
+              print('clicking elevated button for too long');
+            },
+            child: Text('click')));
+  }
+}
