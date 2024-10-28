a9ac2fa1728cd2bb645d95cb65c709e9fc8e41c8
admiral <kartikeyk1776@gmail.com>
2024-10-28 17:28:19 +0530

    about InkWell

--- flutter-lib-main/inkwell.dart
+++ flutter-lib-main/inkwell.dart
@@ -59,7 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   onTap: () {
                     print('clicked on text');
                   },
-                  splashColor: Colors.white,
                   child: const Text(
                     'INDIA',
                     style: TextStyle(
