74960bd1f84b66eb234f92b3f3e9c57473fcaa21
admiral <kartikeyk1776@gmail.com>
2024-11-08 17:00:43 +0530

    about icon widget

---
+++ flutter-lib-main/icon.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Entry point of the Flutter application
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // The root widget of the application, MyApp sets up the basic MaterialApp and theme
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Icon',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(97, 86, 77, 77)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter icon'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // Stateful widget to manage state (like the counter) within the page
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Icon(
+            Icons.alarm,
+            size: 100,
+            color: Colors.amber,
+          ),
+        ));
+  }
+}
