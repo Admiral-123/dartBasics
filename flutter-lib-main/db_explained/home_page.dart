2d1d2fa93e997b079edcf948246c77b5c8d60b30
admiral <kartikeyk1776@gmail.com>
2024-11-22 21:02:29 +0530

    add ui

---
+++ flutter-lib-main/db_explained/home_page.dart
@@ -0,0 +1,142 @@
+import 'package:db_project/data/local/db_helper.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  // controllers
+  TextEditingController title = TextEditingController();
+  TextEditingController desc = TextEditingController();
+
+  List<Map<String, dynamic>> allNotes = [];
+  @override
+  void initState() {
+    super.initState();
+    dbRef = DBHelper.getInstance;
+    getNotes();
+  }
+
+  DBHelper? dbRef;
+  void getNotes() async {
+    allNotes = await dbRef!.getAllNotes();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      // notes visible on ui
+      body: allNotes.isNotEmpty
+          ? ListView.builder(
+              itemCount: allNotes.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text('${allNotes[index][DBHelper.COLUM_NOTE_SNO]}'),
+                  title: Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
+                  subtitle: Text(allNotes[index][DBHelper.COLUMN_NOTE_DESC]),
+                );
+              })
+          : const Center(
+              child: Text('no notes yet'),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // note to be added
+
+          // bool check =
+          //     await dbRef!.addNote(myTitle: "new note", myDesc: "desc");
+          // getNotes();
+          // if (check) {
+          //   getNotes();
+          // }
+
+          // when clicked on floating btn this'll pop up
+          showModalBottomSheet(
+            // elevation: 500,
+            context: context,
+            builder: (context) {
+              return Container(
+                height: 700,
+                padding: const EdgeInsets.all(16.0),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16),
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize:
+                      MainAxisSize.min, // Ensures the modal adjusts its size
+                  children: [
+                    const Text(
+                      'Add Note',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      controller: title,
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      controller: desc,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: 3,
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              title.clear();
+                              desc.clear();
+                            },
+                            child: Text('cancel')),
+                        ElevatedButton(
+                          onPressed: () async {
+                            // Add logic to save the note
+                            if (title.text != '' && desc.text != '') {
+                              await dbRef!.addNote(
+                                  myTitle: title.text.toString(),
+                                  myDesc: desc.text.toString());
+
+                              title.clear();
+                              desc.clear();
+                            }
+                            Navigator.pop(
+                                context); // Close the modal after adding a note
+
+                            getNotes();
+                          },
+                          child: const Text('Save'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
